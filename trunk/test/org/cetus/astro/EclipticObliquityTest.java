12a43faaf671c5f20602613b94d768eed1fb7b9b
inaki.ortizdelandaluce@gmail.com <inaki.ortizdelandaluce@gmail.com@eb099bb5-0fce-7dc9-ce3c-bbd980d98157>
2011-12-29 15:12:10 +0000

    Update unit test after change of method name in AngleUtils class.

--- trunk/test/org/cetus/astro/EclipticObliquityTest.java
+++ trunk/test/org/cetus/astro/EclipticObliquityTest.java
@@ -9,14 +9,14 @@ public class EclipticObliquityTest {
   @Test
   public final void testCalculateMeanObliquityDouble() {
     // Obliquity on 1987 April 10 at 0h TD (2446895.5 JDE) is 23 26' 27".407
-    assertEquals(AngleUtils.sexagesimalToDeg(23, 26, 27.407), 
+    assertEquals(AngleUtils.dmsToDeg(23, 26, 27.407), 
         EclipticObliquity.calculateMeanObliquity(-0.127296372348), 0.00001);
   }
 
   @Test
   public final void testCalculateTrueObliquityDouble() {
     // Obliquity on 1987 April 10 at 0h TD (2446895.5 JDE) is 23 26' 27".407
-    assertEquals(AngleUtils.sexagesimalToDeg(23, 26, 36.850), 
+    assertEquals(AngleUtils.dmsToDeg(23, 26, 36.850), 
         EclipticObliquity.calculateTrueObliquity(-0.127296372348), 0.00001);
   }
 
