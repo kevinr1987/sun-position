bfaa113f10c715acac81b92e18e955b6265e90e2
inaki.ortizdelandaluce <inaki.ortizdelandaluce@gmail.com>
2011-12-29 15:06:58 +0000

    Added a couple of utility methods to format angle decimal values to sexagesimal (hms/dms).

--- src/java/org/cetus/astro/AngleUtils.java
+++ src/java/org/cetus/astro/AngleUtils.java
@@ -17,6 +17,8 @@
  */
 package org.cetus.astro;
 
+import java.text.DecimalFormat;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -29,9 +31,51 @@ public class AngleUtils {
   private static Logger log = Logger.getRootLogger();
 
   public static double hmsToDeg(int hour, int minute, double second) {
-    return sexagesimalToDeg(hour, minute, second) * 15;
+    return dmsToDeg(hour, minute, second) * 15;
   }
   
+  public static double dmsToDeg(int degree, int arcminute,
+      double arcsecond) {
+    return (double) degree + (double) arcminute / 60 + (double) arcsecond
+        / (60 * 60);
+  }
+  
+  public static String formatDegToHms(double degrees, double min, double max) {
+    String s = "";
+    // normalize values within the given range
+    double hours = normalizeAngle(degrees, min , max) / 15.0;
+    // take sign into account
+    if(hours < 0) {
+      hours = Math.abs(hours);
+      s += "-";
+    }
+    // calculate minutes and seconds;
+    double minutes = (hours - (int)hours) * 60.0;
+    double seconds = (minutes - (int)minutes) * 60.0;    
+    s += new DecimalFormat("00").format((int)hours) + "h"
+        + new DecimalFormat("00").format((int)minutes) + "m" 
+        + new DecimalFormat("00.000").format(seconds) + "s";
+    return s;  
+  }
+  
+  public static String formatDegToDms(double degrees, double min,
+      double max) {
+    String s = "";
+    // normalize values within the given range    
+    double deg = normalizeAngle(degrees, min , max);
+    // take sign into account
+    if(deg < 0) {
+      deg = Math.abs(deg);
+      s += "-";
+    }
+    double arcmin = (deg - (int)deg) * 60.0;
+    double arcsec = (arcmin - (int)arcmin) * 60.0;
+    s += new DecimalFormat("00").format((int)deg) + "d" 
+      + new DecimalFormat("00").format((int)arcmin) + "'" 
+      + new DecimalFormat("00.00").format(arcsec) + "\"";
+    return s;
+  }
+
   /**
    * Normalizes an angle value between a given range
    * 
@@ -48,9 +92,4 @@ public class AngleUtils {
     return ((value - minValue) % delta + delta) % delta + minValue;
   }
   
-  public static double sexagesimalToDeg(int degree, int arcminute,
-      double arcsecond) {
-    return (double) degree + (double) arcminute / 60 + (double) arcsecond
-        / (60 * 60);
-  }
 }
